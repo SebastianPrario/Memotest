311f0996a039ac67fe15093f1448e399413752ee
SebastianPrario <121460546+SebastianPrario@users.noreply.github.com>
2023-12-29 19:23:55 -0300

    ajustes en css

--- memotest/src/Json/arrayCard.js
+++ memotest/src/Json/arrayCard.js
@@ -15,13 +15,17 @@ const arrayCard = [
     { number: 14 , image: "https://res.cloudinary.com/dvbb6loxq/image/upload/v1703540818/futbol/a2ffbdc8-389b-4aee-998e-52617060e08e_source-aspect-ratio_default_1060628_ynzdj4.webp" , view: 'hidden' , disable: false , par: 7},
     { number: 15 , image: "https://res.cloudinary.com/dvbb6loxq/image/upload/v1703540822/futbol/a125eabb-2fb6-4042-87a6-b4012188a4b6_source-aspect-ratio_default_1060669_owj68b.webp" , view: 'hidden' , disable: false , par: 8},
     { number: 16 , image: "https://res.cloudinary.com/dvbb6loxq/image/upload/v1703540822/futbol/a125eabb-2fb6-4042-87a6-b4012188a4b6_source-aspect-ratio_default_1060669_owj68b.webp" , view: 'hidden' , disable: false , par: 8},
-    { number: 17 , image: "https://res.cloudinary.com/dvbb6loxq/image/upload/v1703540824/futbol/cce1e7be-34b0-4f55-86c6-76eee69fde17_source-aspect-ratio_default_1060691_krjmxe.webp" , view: 'hidden' , disable: false , par: 9},
-    { number: 18 , image: "https://res.cloudinary.com/dvbb6loxq/image/upload/v1703540824/futbol/cce1e7be-34b0-4f55-86c6-76eee69fde17_source-aspect-ratio_default_1060691_krjmxe.webp" , view: 'hidden' , disable: false , par: 9},
+    { number: 17 , image: "https://res.cloudinary.com/dvbb6loxq/image/upload/v1703540830/futbol/cd95dca7-5c39-4975-b835-e575cea558d7_source-aspect-ratio_default_1060630_mmqrxj.webp" , view: 'hidden' , disable: false , par: 9},
+    { number: 18 , image: "https://res.cloudinary.com/dvbb6loxq/image/upload/v1703540830/futbol/cd95dca7-5c39-4975-b835-e575cea558d7_source-aspect-ratio_default_1060630_mmqrxj.webp" , view: 'hidden' , disable: false , par: 9},
     { number: 19 , image: "https://res.cloudinary.com/dvbb6loxq/image/upload/v1703540827/futbol/e5b99019-7ec4-4e6b-94b3-fac985ac931c_source-aspect-ratio_default_1060685_hxnhjv.webp" , view: 'hidden' , disable: false , par:10},
     { number: 20 , image: "https://res.cloudinary.com/dvbb6loxq/image/upload/v1703540827/futbol/e5b99019-7ec4-4e6b-94b3-fac985ac931c_source-aspect-ratio_default_1060685_hxnhjv.webp" , view: 'hidden' , disable: false , par:10},
- 
-    
 
 ]
+
+
+stumble : {
+
+
+}
 
 export default arrayCard
